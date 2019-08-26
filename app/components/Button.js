9c28db09556bcf29783aac62a40af74175950d28
BuildTools <bscorbitt@wpi.edu>
2019-08-25 23:51:51 -0400

    Revised button component.

--- app/components/Button.js
+++ app/components/Button.js
@@ -6,12 +6,14 @@ class Button extends React.Component {
   getIcon() {
     if (!this.props.icon) return null;
     let icon = this.props.icon;
+    let style = !icon.end ? styles.IconLeft : styles.IconRight;
+    let centered = !this.props.centered? styles.CenteredIcon : null; 
     return (
-      <View style={!icon.end ? styles.IconLeft : styles.IconRight}>
+      <View style={style}>
         <Icon
           type="material-community"
           name={icon.name}
-          style={!icon.end ? styles.IconLeft : styles.IconRight}
+          style={[style, centered]}
           size={icon.size || 40}
           color={!this.props.secondary ? "white" : "rgba(46,70,108,1)"}
         />
@@ -33,15 +35,17 @@ class Button extends React.Component {
   }
 
   render() {
-    let style = !this.props.secondary
-      ? styles.ButtonPrimary
-      : styles.ButtonSecondary;
+    let color = this.props.secondary? styles.Secondary : styles.Primary;
+    let centered = this.props.centered? styles.Centered : null;
+
+
+
     return (
-      <TouchableOpacity onPress={this.props.onPress} style={style}>
+      <TouchableOpacity onPress={this.props.onPress} style={[styles.Button, color]}>
         {this.getStartIcon()}
         <Text
           style={
-            !this.props.secondary ? styles.TextPrimary : styles.TextSecondary
+            [color, styles.Text, centered]
           }
         >
           {this.props.title}
@@ -53,48 +57,53 @@ class Button extends React.Component {
 }
 
 const styles = StyleSheet.create({
-  ButtonPrimary: {
+  Primary: {
     color: "white",
-    backgroundColor: "rgba(46,70,108,1)",
-    textAlign: "center",
-    textAlignVertical: "center",
-    paddingVertical: 10,
-    borderRadius: 2,
-    borderWidth: 1,
-    flexDirection: "row",
-    paddingHorizontal: 10
+    backgroundColor: "rgba(46,70,108,1)"
   },
-  TextPrimary: {
-    color: "white",
-    textAlign: "center",
-    textAlignVertical: "center",
-    fontSize: 18,
-    flex: 5
-  },
-  ButtonSecondary: {
+
+  Secondary: {
     color: "rgba(46,70,108,1)",
     backgroundColor: "#e5ffff",
-    textAlign: "center",
-    textAlignVertical: "center",
+    fontWeight: "bold"
+  },
+
+  Button: {
     paddingVertical: 10,
+    paddingHorizontal: 10,
+    flexDirection: "row",
     borderRadius: 2,
     borderWidth: 1,
-    flexDirection: "row",
-    paddingHorizontal: 10
+    justifyContent: "center"
   },
-  TextSecondary: {
-    color: "rgba(46,70,108,1)",
+
+  Text: {
     textAlign: "center",
     textAlignVertical: "center",
     fontSize: 18,
-    fontWeight: "bold",
-    flex: 5
+    flex: 1,
+    backgroundColor: null
   },
+
+  Centered: {
+    // alignSelf: "",
+    flex: .5,
+    textAlign: "center"
+  },
+
+  CenteredIcon: {
+    flex: 2
+  },
+
   IconLeft: {
-    flex: 1
+    // flex: 1
+    paddingLeft: 10,
+    paddingRight: 15,
   },
   IconRight: {
-    flex: 1
+    // flex: 1
+    paddingLeft: 15,
+    paddingRight: 10
   }
 });
 
