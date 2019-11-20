0dc4595e66b0f251602e038c530fab2d48ea8a27
BuildTools <bscorbitt@wpi.edu>
2019-11-20 11:41:06 -0500

    Changed file serve route to simple proxy

--- server/routes/api/cms/file_serve.js
+++ server/routes/api/cms/file_serve.js
@@ -1,23 +1,14 @@
 const express = require("express");
 const strapi = require("../../../strapi_init");
+var proxy = require('express-http-proxy');
+
 
 const router = express.Router();
 
-router.get("/file/*", async function(req,res,next) {
-  var path = req.originalUrl.substring(req.originalUrl.indexOf("/file/") + 6)
-  if (!path) {
-    console.error("No id found")
-    res.status(404).send();
-    return;
-  }
+router.get("/file/*", proxy(`${strapi.axios.defaults.baseURL}`,{
+  proxyReqPathResolver: function (req) {
+    return req.url.replace('file', 'uploads');
+  }}))
 
-  // Request for CMS file as a stream and pipe its output to response
-  strapi.axios.request({method: 'get', url: `/uploads/${path}`, responseType: 'stream'}).then(response => {
-    response.data.pipe(res);
-  }).catch(err => {
-    console.error(err);
-    res.status(500).send();
-  });
-});
 
 module.exports = router;
