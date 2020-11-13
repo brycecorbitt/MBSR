054d30e75c8bad559fa997dbdd803ff07f567a01
Bryce Corbitt <bscorbitt@wpi.edu>
2020-11-13 15:31:44 -0500

    Improved warning filtration for development

--- app/index.js
+++ app/index.js
@@ -1,11 +1,18 @@
-/**
- * @format
- */
-
 import {AppRegistry, YellowBox} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-// console.disableYellowBox = true;
-YellowBox.ignoreWarnings(['Setting a timer']);
+
+if(__DEV__){
+	const disabled_warnings = ['Setting a timer', 'Warning: componentWillReceiveProps']
+	const {warn} = console
+	// Hack to keep ignored warnings from printing to console
+	console.warn = (...args) => {
+		if(typeof args[0] === 'string' && disabled_warnings.some((prefix) => args[0].startsWith(prefix)))
+			return
+		warn(...args)
+	}
+	//YellowBox overrides console.warn, so ignoreWarnings must be called after for the hack to work
+	YellowBox.ignoreWarnings(disabled_warnings);
+}
 
 AppRegistry.registerComponent(appName, () => App);
