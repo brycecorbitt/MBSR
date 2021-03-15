26da94b99a09ffc44dc5b7c2e74e716d8b183e28
Bryce Corbitt <bscorbitt@wpi.edu>
2021-03-15 12:37:42 -0400

    Manually Linked react-native-device-info to Fix Build Error

--- app/android/app/src/main/java/com/doyoumindful/MainApplication.java
+++ app/android/app/src/main/java/com/doyoumindful/MainApplication.java
@@ -7,6 +7,7 @@ import com.facebook.react.PackageList;
 import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
 import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import org.wonday.orientation.OrientationPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
